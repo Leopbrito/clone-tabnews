bf21a60f579ac5d778fa36a26cb864d4d93951a4
Leonardo Brito <leopbrito@icloud.com>
2025-10-26 13:19:25 -0300

    feat: implementing error handling on `database.js`

--- infra/database.js
+++ infra/database.js
@@ -8,16 +8,23 @@ async function query(queryObject) {
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
   });
-  let result
+  console.log('Database Credentials: ', {
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  })
   try {
     await client.connect();
-    result = await client.query(queryObject);
+    const result = await client.query(queryObject);
+    return result;
   } catch (error) {
-    console.log(error)
+    console.log(error);
+    throw error;
   } finally {
     await client.end();
   }
-  return result;
 }
 
 export default {
